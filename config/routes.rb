7eb9008fa48a9cd4476a201848d00d0fd8595795
Alex Stophel <alexstophel@gmail.com>
2015-11-06 19:54:35 -0500

    Add high_voltage for static pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, skip: ['passwords', 'sessions', 'registrations']
+
+  root 'high_voltage/pages#show', id: 'home'
+
+  authenticated :user do
+    # root for authenticated users goes here.
+    # root 'some_controller#some_page', as: :authenticated_user
+  end
 end
