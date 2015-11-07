f5dd6eb107551975642d4de577f9762cef1191e7
Alex Stophel <alexstophel@gmail.com>
2015-11-06 19:47:40 -0500

    Create User model and added Devise.

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,3 @@
 Rails.application.routes.draw do
+  devise_for :users, skip: ['passwords', 'sessions', 'registrations']
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -15,6 +15,7 @@ RSpec.configure do |config|
   config.fixture_path = '#{::Rails.root}/spec/fixtures'
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
+  config.include Devise::TestHelpers, type: :controller
 end
 
 Shoulda::Matchers.configure do |config|
