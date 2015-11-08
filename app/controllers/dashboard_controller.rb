4a90a32c93076a1ea076356db78b62166274bfc7
Alex Stophel <alexstophel@gmail.com>
2015-11-08 15:31:31 -0500

    Authenticate the dashboard.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class DashboardController < ApplicationController
+  before_filter :authenticate_user!
+
   def show
     search = current_user.repositories.ransack(name_cont: params[:search])
     @repositories = search.result.page(params[:page])
